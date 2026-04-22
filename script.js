e71ff6fd2099d10a0c04c1a116f6e0a8effaf1d8
malishkabhayje07-collab <malishkabhayje07@gmail.com>
2026-04-22 20:31:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function addToCart(name,price){
-  alert(name+" added to cart 🛒");
-}
+function addToCart(name, price) {
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
 
+  let popup = document.createElement("div");
+  popup.innerText = "Added to cart 💖";
+  popup.className = "popup";
+  document.body.appendChild(popup);
+
+  setTimeout(() => popup.remove(), 2000);
+}
 function wishlist(){
   alert("Added to wishlist 💖");
 }
